2a81cc092f897aee6cbc71c116cd049ef60cd5e9
GerganaKraeva <gergana.evg.kraeva@gmail.com>
2024-06-09 00:18:30 +0300

    Add some comments to HomeController

--- PathFinder/src/main/java/bg/softuni/pathfinder/web/HomeController.java
+++ PathFinder/src/main/java/bg/softuni/pathfinder/web/HomeController.java
@@ -3,7 +3,6 @@ package bg.softuni.pathfinder.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Random;
 
